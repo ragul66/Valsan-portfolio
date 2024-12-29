5923eb6cd12444e60cc988de547f75a7a8a75414
ragul66 <ragul1.gokul2.3@gmail.com>
2024-12-29 20:34:35 +0530

    minor changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Head from "next/head";
 
 export const metadata = {
   title: "Valsan-Portfolio",
@@ -9,12 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Add favicon link */}
-        <link rel="shortcut icon" href="/favicon.ico" type="image/png" />
-      </Head>
       <body>{children}</body>
     </html>
   );
